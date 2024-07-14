1abb4da9f3cbf9e5954eb08ae97e999b59a2a166
양진영 <yjy852@gmail.com>
2024-07-14 23:24:36 +0900

    Feat: Add global validation-pipe

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsNumber()
+  likeCount: number;
+
+  @IsNumber()
+  commentCount: number;
+}
