6c9db4291212549885dedec1226755896e8502b5
양진영 <yjy852@gmail.com>
2024-08-06 16:26:03 +0900

    Fix: Create upload directory when importing multer module

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,12 +1,20 @@
-import { BadRequestException, Module } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  Module,
+} from '@nestjs/common';
 import { PaginationService } from './services/pagination.service';
 import { PublicController } from './controllers/public.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { extname } from 'path';
 import * as multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
-import { PUBLIC_TEMP_PATH } from './constants/path.constant';
+import {
+  PUBLIC_POST_IMAGE_PATH,
+  PUBLIC_TEMP_PATH,
+} from './constants/path.constant';
 import { CustomLoggerService } from './services/custom-logger.service';
+import { promises as fs } from 'fs';
 
 @Module({
   imports: [
@@ -27,7 +35,16 @@ import { CustomLoggerService } from './services/custom-logger.service';
         return cb(null, true);
       },
       storage: multer.diskStorage({
-        destination: function (req, file, cb) {
+        destination: async (req, file, cb) => {
+          try {
+            await fs.mkdir(PUBLIC_TEMP_PATH, { recursive: true });
+            await fs.mkdir(PUBLIC_POST_IMAGE_PATH, { recursive: true });
+          } catch (err) {
+            throw new InternalServerErrorException(
+              '업로드 저장 경로를 생성하는데 실패했습니다.',
+            );
+          }
+
           cb(null, PUBLIC_TEMP_PATH);
         },
         filename: function (req, file, cb) {
