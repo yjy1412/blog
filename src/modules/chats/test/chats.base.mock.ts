38cdc9953f5b364239636b0fce4737fe86314b91
양진영 <yjy852@gmail.com>
2024-08-20 16:11:54 +0900

    Test: Move socket mock data for test to base-mock directory

--- src/modules/chats/test/chats.base.mock.ts
+++ src/modules/chats/test/chats.base.mock.ts
@@ -2,28 +2,6 @@ import { BaseMock } from '../../common/test/base.mock';
 import { ChatModel } from '../entities/chats.entity';
 
 export class ChatsBaseMock extends BaseMock {
-  public readonly mockSocketServer = {
-    in: jest.fn().mockReturnValue({
-      emit: jest.fn(),
-    }),
-  };
-
-  public readonly mockSocket = {
-    handshake: {
-      headers: {
-        authorization: null,
-      },
-    },
-    user: this.mockUser,
-    disconnect: jest.fn(),
-    emit: jest.fn(),
-    join: jest.fn(),
-    leave: jest.fn(),
-    to: jest.fn().mockReturnValue({
-      emit: jest.fn(),
-    }),
-  };
-
   public readonly mockChat: ChatModel = {
     id: 1,
     name: 'test room',
--- src/modules/common/test/base.mock.ts
+++ src/modules/common/test/base.mock.ts
@@ -44,4 +44,26 @@ export abstract class BaseMock {
     deletedAt: null,
     authorId: 1,
   };
+
+  public readonly mockSocketServer = {
+    in: jest.fn().mockReturnValue({
+      emit: jest.fn(),
+    }),
+  };
+
+  public readonly mockSocket = {
+    handshake: {
+      headers: {
+        authorization: null,
+      },
+    },
+    user: this.mockUser,
+    disconnect: jest.fn(),
+    emit: jest.fn(),
+    join: jest.fn(),
+    leave: jest.fn(),
+    to: jest.fn().mockReturnValue({
+      emit: jest.fn(),
+    }),
+  };
 }
