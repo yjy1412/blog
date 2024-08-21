5ff25a428abe700e11488c5b3d19fefed6ae6934
양진영 <yjy852@gmail.com>
2024-08-21 17:15:02 +0900

    Feat: Modify exception-filters to catch internal server error

--- src/core/exception-filters/http.exception-filter.ts
+++ src/core/exception-filters/http.exception-filter.ts
@@ -3,34 +3,47 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { CustomLoggerService } from '../../modules/common/services/custom-logger.service';
 
-@Catch(HttpException)
+@Catch(Error, HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  constructor(private readonly customLoggerService: CustomLoggerService) {}
+  constructor(public readonly customLoggerService: CustomLoggerService) {}
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  public readonly internalServerErrorResponse = {
+    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+    message:
+      '서버 장애가 발생했습니다. 장애가 지속되는 경우 관리자에게 문의해주세요.',
+    error: 'Internal Server Error',
+  };
+
+  catch(exception: Error | HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
+    const isHttpException = exception instanceof HttpException;
 
-    if (status === 500) {
+    let status: HttpStatus;
+    let errorResponse: string | object;
+
+    if (
+      isHttpException &&
+      exception.getStatus() !== HttpStatus.INTERNAL_SERVER_ERROR
+    ) {
+      status = exception.getStatus();
+      errorResponse = exception.getResponse();
+    } else {
       this.customLoggerService.error(
         exception.message,
         HttpExceptionFilter.name,
         exception.stack,
       );
 
-      response.status(status).json({
-        statusCode: status,
-        message:
-          '서버 장애가 발생했습니다. 장애가 지속되는 경우 관리자에게 문의해주세요.',
-        error: exception.name,
-      });
-    } else {
-      response.status(status).json(exception.getResponse());
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+      errorResponse = this.internalServerErrorResponse;
     }
+
+    response.status(status).json(errorResponse);
   }
 }
--- src/core/exception-filters/web-socket.http.exception-filter.ts
+++ src/core/exception-filters/web-socket.http.exception-filter.ts
@@ -1,16 +1,17 @@
 import {
   ArgumentsHost,
   Catch,
-  ExceptionFilter,
   HttpException,
+  HttpStatus,
   Injectable,
 } from '@nestjs/common';
 import { CustomLoggerService } from '../../modules/common/services/custom-logger.service';
 import { SocketEventEnum } from '../../modules/common/enums/socket-event.enum';
+import { HttpExceptionFilter } from './http.exception-filter';
 
 @Injectable()
-@Catch(HttpException)
-export class WebSocketHttpExceptionFilter implements ExceptionFilter {
+@Catch(Error, HttpException)
+export class WebSocketHttpExceptionFilter extends HttpExceptionFilter {
   /**
    * [ 주의 ]
    * HttpExceptionFilter와 동일한 응답형식을 갖추고 있습니다.
@@ -19,28 +20,31 @@ export class WebSocketHttpExceptionFilter implements ExceptionFilter {
    * 해당 프로젝트는 모든 Exception을 HttpException으로 처리하고 해당 필터를 통해 exception 메시지를 전달합니다.
    * 그렇게 한 의도는, 서비스 로직에서 처리하는 Exception을 동일하게 하여 프로젝트의 복잡성을 줄이기 위함 입니다.
    */
-  constructor(private readonly customLoggerService: CustomLoggerService) {}
+  constructor(public readonly customLoggerService: CustomLoggerService) {
+    super(customLoggerService);
+  }
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: Error | HttpException, host: ArgumentsHost) {
     const socket = host.switchToWs().getClient();
+    const isHttpException = exception instanceof HttpException;
 
-    const status = exception.getStatus();
+    let errorResponse: string | object;
 
-    if (status === 500) {
+    if (
+      isHttpException &&
+      exception.getStatus() !== HttpStatus.INTERNAL_SERVER_ERROR
+    ) {
+      errorResponse = exception.getResponse();
+    } else {
       this.customLoggerService.error(
         exception.message,
         WebSocketHttpExceptionFilter.name,
         exception.stack,
       );
 
-      socket.emit(SocketEventEnum.EXCEPTION, {
-        statusCode: status,
-        message:
-          '서버 장애가 발생했습니다. 장애가 지속되는 경우 관리자에게 문의해주세요.',
-        error: exception.name,
-      });
-    } else {
-      socket.emit(SocketEventEnum.EXCEPTION, exception.getResponse());
+      errorResponse = this.internalServerErrorResponse;
     }
+
+    socket.emit(SocketEventEnum.EXCEPTION, errorResponse);
   }
 }
